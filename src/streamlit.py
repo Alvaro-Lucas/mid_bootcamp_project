16787d540b7364f1716072ba245cf0808b450fd9
altersaber@hotmail.com <alvaro@Alter.localdomain>
2021-08-04 23:25:31 +0200

    Download pdf añadido y webscraping de comunidades autonomas realizado

--- src/streamlit.py
+++ src/streamlit.py
@@ -10,9 +10,6 @@ import re
 import requests
 import json
 
-class FPDF(FPDF, HTMLMixin):
-    pass
-
 def get_data_db(query):
     url = f"http://127.0.0.1:3500/get"
     res = requests.get(url+f"?{query}")
@@ -24,12 +21,12 @@ def covid_cases_graph(data, periodo=30):
     covid_cases_date = pd.DataFrame(data)
     covid_cases_date = covid_cases_date.drop(columns=["_id", "Lat", "Long"])
     plt.figure(figsize=[20,20])
-
+    columns = [column for column in covid_cases_date.columns[1::periodo]]
     for country in covid_cases_date.values:
-        deaths = [int(death) for death in country[1::periodo]]
-        plt.ylabel('Deaths')
+        cases = [int(case) for case in country[1::periodo]]
+        plt.ylabel('Cases')
         plt.xlabel('Date')
-        plt.plot(covid_cases_date.columns[1::periodo], deaths, label=country[0])
+        plt.plot(columns, cases, label=country[0])
         plt.legend()
 
     return plt
@@ -85,6 +82,37 @@ if chosen:
 
 if st.button('Download PDF'):
     pdf = FPDF()
+    pdf.add_page(orientation="L", format="A3")
+    plt.savefig("Cases_dates.jpg")
+    pdf.image("./Cases_dates.jpg", w=400, h=260)
     pdf.add_page()
+
+    covid_cases_data = pd.DataFrame(data).drop(columns=["_id", "Lat", "Long"])
+    num_case_date = []
+
+    columns = [column for column in covid_cases_data.columns[1::periodo]]
+    columns.insert(0,covid_cases_data.columns[0])
+    for country in covid_cases_data.values: 
+        cases = [int(case) for case in country[1::periodo]]
+        cases.insert(0,country[0])
+        num_case_date.append(cases)
+    
+    pdf.set_font('Arial', 'BIU', 16)
+    pdf.write(16,"Data in JSON format\n")
+    pdf.set_font('Arial', '', 9)
+    for country in range(len(num_case_date)):
+        if country == 0:
+            pdf.write(5, "\n{")
+        pdf.write(5, "\n    {\n")
+        for position in range(len(columns)):
+            if position == len(columns)-1:
+                pdf.write(5, f"        {columns[position]} : {num_case_date[country][position]}\n")
+            else:
+                pdf.write(5, f"        {columns[position]} : {num_case_date[country][position]},\n")
+        if country == len(num_case_date)-1:
+            pdf.write(5, "    }\n")
+            pdf.write(5, "}\n")
+        else:
+            pdf.write(5, "    },")
     
     pdf.output('Covid_Cases_Dashboard.pdf', "I")
